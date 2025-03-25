23bbfe386c49f13a981fd441b0b97818e0ec644a
devopsakhil5 <110562970+devopsakhil5@users.noreply.github.com>
2025-03-25 14:11:31 +0000

    Included VPC and Subnet into the configuration

--- main.tf
+++ main.tf
@@ -30,3 +30,23 @@ resource "aws_iam_user_policy_attachment" "test-attach" {
   user       = aws_iam_user.name.name
   policy_arn = aws_iam_policy.policy.arn
 }
+
+resource "aws_vpc" "myvpc" {
+
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "myvpc"
+  }
+}
+
+resource "aws_subnet" "mysubnet" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "mysubnet"
+  }
+}
+
+
