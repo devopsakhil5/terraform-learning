2f325ed284c1a90b17a0c93fd8fd431a5832c3c1
devopsakhil5 <110562970+devopsakhil5@users.noreply.github.com>
2025-03-25 14:22:56 +0000

    Rough addition of IGW and Route table

--- main.tf
+++ main.tf
@@ -49,4 +49,29 @@ resource "aws_subnet" "mysubnet" {
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
 
+  tags = {
+    Name = "myIGW"
+  }
+}
+
+resource "aws_internet_gateway_attachment" "myigw" {
+  internet_gateway_id = aws_internet_gateway.igw.id
+  vpc_id              = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.mysubnet.id
+  route_table_id = aws_route_table.rt.id
+}
